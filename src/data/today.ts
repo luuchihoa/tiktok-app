b8e92aef2f8d29a951f481c3556c6ecb9bc1d41c
leenguyenzz <leenguyen.nll@gmail.com>
2026-08-20 10:23:49 +0700

    chore: update video data 10:23:49 20/8/2026

--- src/data/today.ts
+++ src/data/today.ts
@@ -1,17 +1,16 @@
 import { VideoInput } from "./videoInput";
 
 export const videoData: VideoInput = {
-  "date": "",
-  "bannerTag": "",
-  "feastName": "",
+  "date": "20/08/2026",
+  "bannerTag": "Lễ Nhớ Bắt Buộc",
+  "feastName": "Thánh Bênađô, viện phụ, tiến sĩ Hội Thánh",
   "readingType": "Bài Đọc 1",
   "bibleRef": "Ed 36,23-28",
-  "quote": "",
+  "quote": "\"Ta sẽ ban tặng các ngươi một quả tim mới, sẽ đặt thần khí mới vào lòng các ngươi.\"",
   "audioFile": "current_audio.mp3",
   "introAudioFile": "piano_intro.mp3",
   "outroAudioFile": "piano_outro.mp3",
-  "imageFile": "cross.jpg",
-  "logoFile": "logo.png",
+  "imageFile": "http://localhost:3001/current_image.jpg",
   "imagePositionY": 50,
   "season": "EASTER_CHRISTMAS",
   "websiteUrl": "loichuamoingay.org"
