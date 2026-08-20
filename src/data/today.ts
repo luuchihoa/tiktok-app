41e25207d862862efa90593330e6d05c0e344892
leenguyenzz <leenguyen.nll@gmail.com>
2026-08-20 09:03:08 +0700

    fix: include logoFile in today.ts for clean TS build

--- src/data/today.ts
+++ src/data/today.ts
@@ -11,6 +11,7 @@ export const videoData: VideoInput = {
   "introAudioFile": "piano_intro.mp3",
   "outroAudioFile": "piano_outro.mp3",
   "imageFile": "cross.jpg",
+  "logoFile": "logo.png",
   "imagePositionY": 50,
   "season": "EASTER_CHRISTMAS",
   "websiteUrl": "loichuamoingay.org"
